08e91e583ad6a39b4144c72265a67090c5239a70
LuisAMG22 <luis.mguerrero@alumnos.upm.es>
2023-06-03 03:16:03 +0200

    cambiado principal para no volver atras

--- java/com/example/proyecto/Activity_principal.java
+++ java/com/example/proyecto/Activity_principal.java
@@ -1,10 +1,12 @@
  package com.example.proyecto;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -27,6 +29,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectListener);
 
         loadFragment(firstFragment);
+
+
     }
     private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -54,4 +58,31 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
         transaction.replace(R.id.frame_container,fragment);
         transaction.commit();
     }
-}
+
+     @Override
+     public void onBackPressed() {
+         AlertDialog.Builder builder = new AlertDialog.Builder(this);
+         builder.setTitle("Cerrar aplicación");
+         builder.setMessage("¿Estás seguro de que quieres cerrar la aplicación?");
+         builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+             @Override
+             public void onClick(DialogInterface dialog, int which) {
+                 // Cierra todas las actividades y sale completamente de la aplicación
+                 finishAffinity();
+             }
+         });
+
+         builder.setNegativeButton("Cancelar", null);
+
+         AlertDialog dialog = builder.create();
+         dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+             @Override
+             public void onShow(DialogInterface dialogInterface) {
+                 dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.black));
+                 dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.black));
+             }
+         });
+         dialog.show();
+     }
+
+ }
