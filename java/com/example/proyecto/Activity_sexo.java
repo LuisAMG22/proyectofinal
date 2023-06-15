598c47b578ee06061313a2c84581f5783117a7b3
LuisAMG22 <115694582+LuisAMG22@users.noreply.github.com>
2023-06-15 11:12:05 +0200

    java actualizado 15/06/2023
    java actualizado 15/06/2023

---
+++ java/com/example/proyecto/Activity_sexo.java
@@ -0,0 +1,84 @@
+package com.example.proyecto;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_sexo extends AppCompatActivity {
+
+    private int progress = 0;
+    private ImageView image1, image2;
+    private TextView textView;
+    private ProgressBar progressBar;
+    private Button nextButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sexo);
+
+        progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.textView);
+        image1 = findViewById(R.id.hombre);
+        image2 = findViewById(R.id.mujer);
+        nextButton = findViewById(R.id.nextButton);
+
+        nextButton.setEnabled(false); // Deshabilitar el botón por defecto
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity_sexo.this, Activity_areas_cuerpo.class);
+
+                if (image1.getScaleX() > image2.getScaleX()) {
+                    intent.putExtra("sexo", "hombre");
+                } else {
+                    intent.putExtra("sexo", "mujer");
+                }
+
+                startActivity(intent);
+            }
+        });
+
+
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Acción cuando se hace clic en la imagen 1
+                Toast.makeText(getApplicationContext(), "Seleccion Hombre", Toast.LENGTH_SHORT).show();
+                image1.setScaleX(1.2f); // Aumentar la escala de la imagen 1
+                image1.setScaleY(1.2f);
+                image2.setScaleX(0.8f); // Restablecer la escala de la imagen 2
+                image2.setScaleY(0.8f);
+
+                nextButton.setEnabled(true); // Habilitar el botón
+
+            }
+        });
+
+        image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Acción cuando se hace clic en la imagen 2
+                Toast.makeText(getApplicationContext(), "Seleccion Mujer", Toast.LENGTH_SHORT).show();
+                // Aumentar escala de la imagen 2 (mujer)
+                image2.setScaleX(1.2f);
+                image2.setScaleY(1.2f);
+
+                // Reducir escala de la imagen 1 (hombre)
+                image1.setScaleX(0.8f);
+                image1.setScaleY(0.8f);
+                nextButton.setEnabled(true); // Habilitar el botón
+
+            }
+        });
+    }
+}
+
---
+++ java/com/example/proyecto/FirstFragment.java
@@ -0,0 +1,141 @@
+package com.example.proyecto;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+    private TextView titleTextView;
+    private EditText searchEditText;
+    private ListView exerciseListView;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    public static FirstFragment newInstance(String param1, String param2) {
+        FirstFragment fragment = new FirstFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        titleTextView = view.findViewById(R.id.titleTextView);
+        searchEditText = view.findViewById(R.id.searchEditText);
+        exerciseListView = view.findViewById(R.id.exerciseListView);
+        ImageView searchIcon = view.findViewById(R.id.searchIcon);
+
+        // Configurar el ListView con datos de ejemplo
+        String[] exerciseNames = {"Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4", "Ejercicio 5", "Ejercicio 6", "Ejercicio 7"};
+        int[] exerciseImages = {R.drawable.cuerpo_brazos, R.drawable.cuerpo_brazos_pecho, R.drawable.cuerpo_hombros, R.drawable.cuerpo_hombros, R.drawable.cuerpo_hombros, R.drawable.cuerpo_hombros, R.drawable.cuerpo_hombros};
+
+        final ExerciseListAdapter adapter = new ExerciseListAdapter(getActivity(), exerciseNames, exerciseImages);
+        exerciseListView.setAdapter(adapter);
+
+        // Configurar el evento de clic en el elemento del ListView
+        exerciseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Obtener el ejercicio seleccionado
+                String exerciseName = exerciseNames[position];
+
+                // Abrir una nueva actividad o diálogo para mostrar la información del ejercicio
+                // según tu requerimiento
+                showBottomDialog();
+            }
+        });
+
+
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (searchEditText.getVisibility() == View.VISIBLE) {
+                    searchEditText.setVisibility(View.GONE); // Oculta el EditText de búsqueda si ya está visible
+                } else {
+                    searchEditText.setVisibility(View.VISIBLE); // Muestra el EditText de búsqueda si está oculto
+                }
+            }
+        });
+
+        // Agregar el evento de texto cambiado para el EditText de búsqueda
+        searchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // No se requiere implementación
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Filtrar los elementos del ListView según la consulta de búsqueda
+                adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // No se requiere implementación
+            }
+        });
+
+
+        return view;
+    }
+
+    private void showBottomDialog() {
+
+        final Dialog dialog = new Dialog(this.getContext());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.bottomsheetlayout);
+
+        ImageView cancelButton = dialog.findViewById(R.id.cancelButton);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+    }
+}
