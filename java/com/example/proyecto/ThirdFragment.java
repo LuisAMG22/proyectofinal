3f8440a259b6ca73f6e0a08e76a19c87189b9f63
LuisAMG22 <115694582+LuisAMG22@users.noreply.github.com>
2023-06-16 13:04:14 +0200

    actualizado

--- java/com/example/proyecto/ThirdFragment.java
+++ java/com/example/proyecto/ThirdFragment.java
@@ -10,7 +10,8 @@ import androidx.fragment.app.Fragment;
 
 public class ThirdFragment extends Fragment {
 
-    private WebView webView;
+    private WebView webView1;
+    private WebView webView2;
 
     public ThirdFragment() {
         // Required empty public constructor
@@ -25,36 +26,92 @@ public class ThirdFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_third, container, false);
 
-        webView = rootView.findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
+        webView1 = rootView.findViewById(R.id.webView1);
+        webView1.getSettings().setJavaScriptEnabled(true);
 
-        String html = "<html><head>" +
-                "<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>" +
-                "<script type=\"text/javascript\">" +
-                "google.charts.load('current', {packages: ['corechart']});" +
-                "google.charts.setOnLoadCallback(drawChart);" +
-                "function drawChart() {" +
-                "  var data = google.visualization.arrayToDataTable([" +
-                "    ['Year', 'Sales', 'Expenses']," +
-                "    ['2014', 1000, 400]," +
-                "    ['2015', 1170, 460]," +
-                "    ['2016', 660, 1120]," +
-                "    ['2017', 1030, 540]" +
-                "  ]);" +
-                "  var options = {" +
-                "    title: 'Company Performance'," +
-                "    curveType: 'function'," +
-                "    legend: { position: 'bottom' }" +
-                "  };" +
-                "  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));" +
-                "  chart.draw(data, options);" +
-                "}" +
-                "</script></head><body>" +
-                "<div id=\"chart_div\" style=\"width: 100%; height: 100%;\"></div>" +
-                "</body></html>";
+        webView2 = rootView.findViewById(R.id.webView2);
+        webView2.getSettings().setJavaScriptEnabled(true);
 
-        webView.loadData(html, "text/html", "UTF-8");
+        String[] daysOfWeek = {"Mon", "Tue", "Wed", "Thu", "Fri","Sat","Sun"};
+        int[] dataPointsX = {10, 20, 15, 12, 18, 0, 9};
+
+        String[] categories = {"Category 1", "Category 2", "Category 3"};
+        int[] dataPoints = {30, 40, 30};
+
+        String html1 = generateChartHtml(daysOfWeek, dataPointsX);
+        String html2 = generateDonutChartHtml(categories, dataPoints);
+
+        webView1.loadData(html1, "text/html", "UTF-8");
+        webView2.loadData(html2, "text/html", "UTF-8");
 
         return rootView;
     }
+
+    private String generateDonutChartHtml(String[] categories, int[] dataPoints) {
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("<html><head>")
+                .append("<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>")
+                .append("<script type=\"text/javascript\">")
+                .append("google.charts.load('current', {packages: ['corechart']});")
+                .append("google.charts.setOnLoadCallback(drawChart);")
+                .append("function drawChart() {")
+                .append("  var data = new google.visualization.DataTable();")
+                .append("  data.addColumn('string', 'Category');")
+                .append("  data.addColumn('number', 'Value');")
+                .append("  data.addRows([");
+
+        for (int i = 0; i < categories.length; i++) {
+            htmlBuilder.append("    ['").append(categories[i]).append("', ").append(dataPoints[i]).append("],");
+        }
+
+        htmlBuilder.append("  ]);")
+                .append("  var options = {")
+                .append("    title: 'Data Distribution',")
+                .append("    pieHole: 0.4,") // Controla el tamaño del agujero central
+                .append("    legend: { position: 'bottom' }")
+                .append("  };")
+                .append("  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));")
+                .append("  chart.draw(data, options);")
+                .append("}")
+                .append("</script></head><body>")
+                .append("<div id=\"chart_div\" style=\"width: 100%; height: 100%;\"></div>")
+                .append("</body></html>");
+
+        return htmlBuilder.toString();
+    }
+
+    private String generateChartHtml(String[] xAxisLabels, int[] dataPoints) {
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("<html><head>")
+                .append("<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>")
+                .append("<script type=\"text/javascript\">")
+                .append("google.charts.load('current', {packages: ['corechart']});")
+                .append("google.charts.setOnLoadCallback(drawChart);")
+                .append("function drawChart() {")
+                .append("  var data = new google.visualization.DataTable();")
+                .append("  data.addColumn('string', 'Day of the Week');")
+                .append("  data.addColumn('number', 'Data Point');")
+                .append("  data.addRows([");
+
+        for (int i = 0; i < xAxisLabels.length; i++) {
+            htmlBuilder.append("    ['").append(xAxisLabels[i]).append("', ").append(dataPoints[i]).append("],");
+        }
+
+        htmlBuilder.append("  ]);")
+                .append("  var options = {")
+                .append("    title: 'Data Points',")
+                .append("    curveType: 'function',")
+                .append("    legend: { position: 'bottom' }")
+                .append("  };")
+                .append("  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));")
+                .append("  chart.draw(data, options);")
+                .append("}")
+                .append("</script></head><body>")
+                .append("<div id=\"chart_div\" style=\"width: 100%; height: 100%;\"></div>")
+                .append("</body></html>");
+
+        return htmlBuilder.toString();
+    }
+
 }
+
